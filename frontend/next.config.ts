0fb16b3bf8c1b5c11c0047a96f7b2c1be114a07f
sakincse21 <saleheen.sakin@gmail.com>
2026-04-12 23:00:28 +0600

    vercel json added

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const backendBaseUrl =
+      process.env.BACKEND_API_BASE_URL || "http://localhost:5000";
+
     return [
       {
         source: "/api/v1/:path*",
-        destination: `http://localhost:5000/api/v1/:path*`,
+        destination: `${backendBaseUrl}/api/v1/:path*`,
       },
     ];
   },
